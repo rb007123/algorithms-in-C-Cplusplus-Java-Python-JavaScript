5cd86cca2b16e9d1f94cb7cd6e7e572ab303ffc5
Rishabh Bansal <rishab.bansal400@gmail.com>
2018-10-12 20:53:55 +0530

    Update quicksort.c

--- sorting/quicksort.c
+++ sorting/quicksort.c
@@ -1,19 +1,14 @@
 
-#include<stdio.h> 
-  
-// A utility function to swap two elements 
+#include<iostream.h>
+#include<conio.h>
+
 void swap(int* a, int* b) 
 { 
     int t = *a; 
     *a = *b; 
     *b = t; 
 } 
-  
-/* This function takes last element as pivot, places 
-   the pivot element at its correct position in sorted 
-    array, and places all smaller (smaller than pivot) 
-   to left of pivot and all greater elements to right 
-   of pivot */
+
 int partition (int arr[], int low, int high) 
 { 
     int pivot = arr[high];    // pivot 
@@ -21,11 +16,9 @@ int partition (int arr[], int low, int high)
   
     for (int j = low; j <= high- 1; j++) 
     { 
-        // If current element is smaller than or 
-        // equal to pivot 
         if (arr[j] <= pivot) 
         { 
-            i++;    // increment index of smaller element 
+            i++; 
             swap(&arr[i], &arr[j]); 
         } 
     } 
@@ -33,30 +26,21 @@ int partition (int arr[], int low, int high)
     return (i + 1); 
 } 
   
-/* The main function that implements QuickSort 
- arr[] --> Array to be sorted, 
-  low  --> Starting index, 
-  high  --> Ending index */
 void quickSort(int arr[], int low, int high) 
 { 
     if (low < high) 
     { 
-        /* pi is partitioning index, arr[p] is now 
-           at right place */
         int pi = partition(arr, low, high); 
-  
-        // Separately sort elements before 
-        // partition and after partition 
+
         quickSort(arr, low, pi - 1); 
         quickSort(arr, pi + 1, high); 
     } 
 } 
-  
-/* Function to print an array */
+
 void printArray(int arr[], int size) 
 { 
     int i; 
     for (i=0; i < size; i++) 
-        printf("%d ", arr[i]); 
-    printf("n"); 
+        cout<<arr[i]; 
+    cout<<n; 
 } 
