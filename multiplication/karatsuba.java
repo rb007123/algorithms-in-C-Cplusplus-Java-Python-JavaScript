f7601704a44dc5976607204d956bce69ebf14496
rb007123 <rishab.bansal400@gmail.com>
2018-10-12 20:09:21 +0530

    Update karatsuba.java

--- multiplication/karatsuba.java
+++ multiplication/karatsuba.java
@@ -1,22 +1,38 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.print(karatsuba (923,1723));
+#include <string>
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+class Main
+{
+  static void main(std::vector<std::wstring> &args);
+  private:
+  static int karatsuba(int x, int y);
+};
+
+
+void Main::main(std::vector<std::wstring> &args)
+{
+  std::wcout << karatsuba(923,1723);
+}
+
+int Main::karatsuba(int x, int y)
+{
+  if (std::to_wstring(x)->length() == 1 || (std::to_wstring(y)->length() == 1))
+  {
+	  return x*y;
   }
-  private static int karatsuba(int x, int y){
-    if (String.valueOf(x).length()==1 || (String.valueOf(y).length()==1)){
-		return x*y;
-    }
-	  else{
-		int n = Math.max(String.valueOf(x).length(),String.valueOf(y).length());
-		int z = n / 2;
-		int a = x /(int) Math.pow(10, z);
-		int b = x %(int) Math.pow(10, z);
-		int c = y /(int) Math.pow(10, z);
-		int d = y %(int) Math.pow(10, z);
-		int ac = karatsuba(a,c);
-		int bd = karatsuba(b,d);
-		int adbc = karatsuba(a+b,c+d) - ac - bd;
-		return ac * (int)Math.pow(10, (2*z)) + (adbc * (int)Math.pow(10, z)) + bd;
-    }
-  }
+	else
+	{
+	  int n = std::max(std::to_wstring(x)->length(),std::to_wstring(y)->length());
+	  int z = n / 2;
+	  int a = x / static_cast<int>(std::pow(10, z));
+	  int b = x % static_cast<int>(std::pow(10, z));
+	  int c = y / static_cast<int>(std::pow(10, z));
+	  int d = y % static_cast<int>(std::pow(10, z));
+	  int ac = karatsuba(a,c);
+	  int bd = karatsuba(b,d);
+	  int adbc = karatsuba(a + b,c + d) - ac - bd;
+	  return ac * static_cast<int>(std::pow(10, (2*z))) + (adbc * static_cast<int>(std::pow(10, z))) + bd;
+	}
 }
