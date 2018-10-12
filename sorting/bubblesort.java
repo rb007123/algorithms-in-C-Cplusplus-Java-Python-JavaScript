0be7f1136cc756241a1a3befc6082c0c0169db7a
rb007123 <rishab.bansal400@gmail.com>
2018-10-12 19:44:43 +0530

    Update bubblesort.java

--- sorting/bubblesort.java
+++ sorting/bubblesort.java
@@ -1,48 +1,65 @@
-import java.util.*;
-public class Main {
-   static Scanner scn = new Scanner(System.in);
+#include <string>
+#include <vector>
+#include <iostream>
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] arr = takeInput();
+class Main
+{
+   public:
+   static Scanner *scn;
 
-	    bubblesort(arr);
-	    display(arr);
-	}
-	public static int[] takeInput() {
-	//	System.out.println("enter size of array");
-		int n = scn.nextInt();
-		int[] arr = new int[n];
-	//	System.out.println("enetr values in array");
-		for (int i = 0; i < n; i++) {
-			arr[i] = scn.nextInt();
-		}
-		return arr;
-	}
+	static void main(std::vector<std::wstring> &args);
+	static std::vector<int> takeInput();
 
-	public static void display(int[] arr)
+	static void display(std::vector<int> &arr);
 
+	static void bubblesort(std::vector<int> &arr);
+};
+
+
+Scanner *Main::scn = new Scanner(System::in);
+
+void Main::main(std::vector<std::wstring> &args)
+{
+	std::vector<int> arr = takeInput();
+
+	bubblesort(arr);
+	display(arr);
+}
+
+std::vector<int> Main::takeInput()
+{
+	int n = scn->nextInt();
+	std::vector<int> arr(n);
+
+	for (int i = 0; i < n; i++)
 	{
-	//	System.out.println("array is:");
-
-		for (int val : arr) {
-			System.out.println(val);
-		}
+		arr[i] = scn->nextInt();
 	}
-	
-	public static void bubblesort(int[] arr)
+	return arr;
+}
+
+void Main::display(std::vector<int> &arr)
+
+{
+
+	for (auto val : arr)
 	{
-	    for(int i=0;i<arr.length-1;i++)
-	    {
-	        for(int j=0;j<arr.length-1-i;j++)
-	        {
-	            if(arr[j]>arr[j+1])
-	            {
-	                int temp=arr[j];
-	                arr[j]=arr[j+1];
-	                arr[j+1]=temp;
-	            }
-	        }
-	    }
+		std::wcout << val << std::endl;
+	}
+}
+
+void Main::bubblesort(std::vector<int> &arr)
+{
+	for (int i = 0;i < arr.size() - 1;i++)
+	{
+		for (int j = 0;j < arr.size() - 1 - i;j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
 	}
 }
